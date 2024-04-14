5f8ef71f6cb4e002ce10bb9899a77f8108bd8a7c
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-04-14 14:15:57 +0900

    format by prettier

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Elastic Beanstalk!');
+app.get("/", (req, res) => {
+  res.send("Hello, Elastic Beanstalk!");
 });
 
 app.listen(port, () => {
