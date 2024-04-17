b0389b07bd4f01321637eeb58bf437cdbb975b1d
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-04-17 20:27:59 +0900

    change res.send to res.end

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Elastic Beanstalk!");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello express\n");
 });
 
 app.listen(port, () => {
