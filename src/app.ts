4bb209342a37ee2db6302c68812e28b1bd0d6266
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-04-18 08:18:04 +0900

    Add users api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import userController from "./controllers/userController";
+
+const prisma = new PrismaClient();
 
 const app = express();
+app.use(express.json());
+
+app.use("/users", userController);
 
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
