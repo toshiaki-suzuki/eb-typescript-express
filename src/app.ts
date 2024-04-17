0454b046d08e5e123757036d56cbcf2175dae117
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-04-17 20:29:25 +0900

    Move app listen

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express from "express";
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("hello express\n");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+export default app;
