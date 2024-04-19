3f2cdf036db702e06a87e10051100a69a467702d
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-04-19 19:12:08 +0900

    Add CRUD in user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -11,4 +11,48 @@ router.get("/", async (req: Request, res: Response) => {
   res.json({ users });
 });
 
+// POST /users
+router.post("/", async (req: Request, res: Response) => {
+  const { email, name } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+    },
+  });
+  res.json({ user });
+});
+
+// GET /users/:id
+router.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    where: { id: Number(id) },
+  });
+  res.json({ user });
+});
+
+// PUT /users/:id
+router.put("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { email, name } = req.body;
+  const user = await prisma.user.update({
+    where: { id: Number(id) },
+    data: {
+      email,
+      name,
+    },
+  });
+  res.json({ user });
+});
+
+// DELETE /users/:id
+router.delete("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = await prisma.user.delete({
+    where: { id: Number(id) },
+  });
+  res.json({ user });
+});
+
 export default router;
